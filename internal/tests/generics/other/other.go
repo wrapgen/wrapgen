ded68393fb1f5cf2e5d44973d55af3f9f32d9926
Yves Fischer <yves@tutti.ch>
2024-06-25 16:07:56 +0200

    pro-forma import mockgen from https://github.com/uber-go/mock/tree/v0.4.0

---
+++ internal/tests/generics/other/other.go
@@ -0,0 +1,18 @@
+package other
+
+type One[T any] struct{}
+
+type Two[T any, R any] struct{}
+
+type Three struct{}
+
+type Four struct{}
+
+type Five any
+
+type Either[T, R, K, V any] interface {
+	First() T
+	Second() R
+	Third() K
+	Fourth() V
+}
---
+++ internal/tests/generics/source/mock_external_test.go
@@ -0,0 +1,38 @@
+package source
+
+import (
+	"context"
+	"testing"
+
+	"go.uber.org/mock/gomock"
+	"go.uber.org/mock/mockgen/internal/tests/generics"
+)
+
+func TestMockEmbeddingIface_One(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	m := NewMockEmbeddingIface[int, float64](ctrl)
+	m.EXPECT().One("foo").Return("bar")
+	if v := m.One("foo"); v != "bar" {
+		t.Errorf("One() = %v, want %v", v, "bar")
+	}
+}
+
+func TestMockUniverse_Water(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	m := NewMockUniverse[int](ctrl)
+	m.EXPECT().Water(1024)
+	m.Water(1024)
+}
+
+func TestNewMockGroup_Join(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	m := NewMockGroup[generics.Generator[any]](ctrl)
+	ctx := context.TODO()
+	m.EXPECT().Join(ctx).Return(nil)
+	if v := m.Join(ctx); v != nil {
+		t.Errorf("Join() = %v, want %v", v, nil)
+	}
+}
